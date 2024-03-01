f3b5e6d872a522575caca186beb4c1bfaa21cfd4
accasasola <accasasola@up.edu.ph>
2024-03-01 19:50:28 +0800

    corrected the validatepassword function and storepassword function

--- exer3/casasola_exer3.js
+++ exer3/casasola_exer3.js
@@ -1,27 +1,50 @@
-const passwords = [{ name1: 'Armie', pass1: 'Hello', pass2: 'Hello1234' }]
+//Armie Casasola UV-3L
+//01/03/2024
+// This simple JavaScript Program compares two passwords; if the passwords are the same it returns
+// a new password which is the reverse of the passwords, if the passwords are the same it returns a new password
 
 //function that validates two passwords
-function checkpass(x, y) {
-    result = x.localeCompare(y)
-    if (result == 0) {
-        console.log(pass1 + pass2)
-    } else {
-        console.log(pass1 + pass2)
+function validatePassword(pass1, pass2) {
+    // Check if passwords match
+    if (pass1 !== pass2) {
+        return false;
     }
+    // Check if password length is at least 8 characters
+    if (password1.length < 8) {
+        return false;
+    }
+    // Check if password contains at least 1 number, 1 uppercase character, and 1 lowercase character
+    const containsNumber = /\d/.test(pass1);
+    const containsLowercase = /[a-z]/.test(pass1);
+    const containsUppercase = /[A-Z]/.test(pass1);
+
+    if (!(containsNumber && containsLowercase && containsUppercase)) {
+        return false;
+    }
+    // If all conditions pass, return true
+    return true;
 }
 
 //function for reversing the passwords
 
-function reverse(str) {
-    let r = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        r += str[i];
+function reversePassword(password) {
+    let reversed = '';
+    for (let i = password.length - 1; i >= 0; i--) {
+        reversed += password[i];
     }
-    return r;
+    return reversed;
 }
-console.log(passwords(0, 2))
+
 
 //function for storing the password to the object
-function store(x, y, z) {
-    const xyz = [{ name1: "Armie", pass1: "Hello", pass2: "Hello1234" }]
-} console.log(xyz)
+function storePassword(name, pass1, pass2) {
+    const isValid = validatePassword(pass1, pass2);
+    const newPassword = isValid ? reversePassword(pass1) : "Pass123";
+    return { name: name, newpassword: newPassword };
+}
+
+
+console.log(storePasswordToObject("John", "Hello1234", "Hello1234"));
+
+
+console.log(storePasswordToObject("Armie", "password", "password"));

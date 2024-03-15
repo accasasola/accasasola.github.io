07b7113de87e2567b8aa9b9be15d06f3986ea8d1
accasasola <accasasola@up.edu.ph>
2024-03-15 18:22:21 +0800

    corrected an errors from server.js and request.js

--- exer5/request.js
+++ exer5/request.js
@@ -1,27 +1,60 @@
 import needle from 'needle';
 
+
 needle.get('http://localhost:3000/find-by-isbn-author', {
-    book: 'Everyday',
-    isbn: '9780307931894'
-},
-    (err, res) => {
+    isbn: '9780307931894',
+    author: 'David Levithan'
+}, (err, res) => {
+    if (err) {
+        console.error(err);
+    } else {
         console.log(res.body);
-    });
+    }
+});
 
 needle.get('http://localhost:3000/find-by-author', {
     author: 'David Levithan'
 }, (err, res) => {
-    console.log(res.body);
-});
-needle.post(
-    'http://localhost:3000/add-book',
-    {
-        book: 'Everyday',
-        isbn: '9780307931894',
-        author: 'David Levithan',
-        year: '2012'
-    },
-    (err, res) => {
-        console.log(res.body)
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(res.body);
     }
-);
+});
+
+needle.post('http://localhost:3000/add-book', {
+    name: 'Everyday',
+    isbn: '9780307931894',
+    author: 'David Levithan',
+    year: '2012'
+}, (err, res) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(res.body);
+    }
+});
+needle.post('http://localhost:3000/add-book', {
+    name: 'Will, Grayson,Will Grayson',
+    isbn: '9789510429167',
+    author: 'David Levithan',
+    year: '2010'
+}, (err, res) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(res.body);
+    }
+});
+needle.post('http://localhost:3000/add-book', {
+    name: 'We Were Liars',
+    isbn: '9780385390095',
+    author: 'E. Lockhart',
+    year: '2014'
+}, (err, res) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(res.body);
+    }
+});
--- exer5/server.js
+++ exer5/server.js
@@ -3,49 +3,43 @@ import fs from 'node:fs';
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
+app.use(express.urlencoded({extended: false}));
 
 
 app.post('/add-book', (req, res) => {
     const { name, isbn, author, year } = req.body;
-    if (name && isbn && author && year) {
-        const bookData = `Everyday,9783737343763,David Levithan,2012\n`;
-        fs.appendFile('books.txt', bookData, (err) => {
-            if (err) {
-                console.error(err);
-                res.json({ success: false });
-            } else {
-                console.log('Book added:', bookData);
-                res.json({ success: true });
-            }
-        });
-    } else {
-        res.json({ success: false });
+    if (!name || !isbn || !author || !year) {
+        return res.status(400).json({ success: false, error: 'Missing or empty fields' });
     }
+    const bookData = `${name},${isbn},${author},${year}\n`;
+    fs.appendFile('books.txt', bookData, (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ success: false, error: 'Failed to add book' });
+        }
+        console.log('Book added:', bookData);
+        res.json({ success: true });
+    });
 });
 
 app.get('/find-by-isbn-author', (req, res) => {
-    const { isbn, author } = req.query;
-    if (isbn && author) {
-        res.json({ isbn, author });
-    } else {
-        res.status(400).json({ error: 'Missing parameters' });
-    }
+    fs.readFile('books.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.send(data);
+    });
 });
-
 app.get('/find-by-author', (req, res) => {
-    const { author } = req.query;
-    if (author) {
-
-        res.json({ author });
-    } else {
-        res.status(400).json({ error: 'Missing parameters' });
-    }
+    fs.readFile('books.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        res.send(data);
+    });
 });
 
-
-
-app.listen(3000, () => { console.log('Server started at port 3000') });
-
-
+const PORT = 3000;
+app.listen(PORT, () => { console.log(`Server started at port ${PORT}`); });
